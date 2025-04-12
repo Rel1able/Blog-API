a4b1749475b344544a13e1111705e57c45579f9a
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-04-12 11:04:49 +0200

    fixed cors enabling position

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const postsRouter = require("./routes/postsRouter");
 const commentsRouter = require("./routes/commentsRouter");
 require("./config/passport");
 const cors = require("cors");
-
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(authRouter);
 app.use("/posts", postsRouter);
 app.use("/posts", commentsRouter)
-app.use(cors());
+
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
