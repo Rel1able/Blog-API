d815c7b19bbd89fa97ff48b66b2a022e89f7cb21
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-04-13 20:41:46 +0200

    added user data alongside the jsonwebtoken

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,12 +16,26 @@ async function createUser(req, res, next) {
 
 
 async function createToken(req, res) {
-    const token = jwt.sign({id: req.user.id, username: req.user.username}, JWT_SECRET)
-    res.status(200).json({token});
+    const user = {
+        id: req.user.id,
+        username: req.user.username,
+        role: req.user.role
+    }
+    const token = jwt.sign({id: user.id, username: user.username, role: user.role}, JWT_SECRET)
+    res.status(200).json({token, user});
 }
+
+async function handleLogout(req, res, next) {
+    req.logout((err) => {
+        return next(err);
+    })
+    res.json({msg: "You was successfully logged out"})
+}
+
     
 module.exports = {
     createUser,
     createToken,
+    handleLogout
 
 }
